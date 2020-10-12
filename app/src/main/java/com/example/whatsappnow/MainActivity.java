4ba1ecdc1f516ced581e869d9f7e16a1d51bf1d6
zohky <zohky0528@gmail.com>
2020-10-12 22:04:59 +0300

    Done basic functionality.

--- app/src/main/java/com/example/whatsappnow/MainActivity.java
+++ app/src/main/java/com/example/whatsappnow/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.whatsappnow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void sendMessage(View view) {
+
+        EditText phoneNumber = (EditText)findViewById(R.id.editText2);
+        EditText message = (EditText) findViewById(R.id.editText);
+
+        String phone = phoneNumber.getText().toString();
+        String mess = message.getText().toString();
+
+        phone = phone.substring(1);
+
+
+        String url = "https://wa.me/972" + phone + "?text=" + mess;
+
+//        Intent i = new Intent(Intent.ACTION_VIEW);
+//        i.setData(Uri.parse(url));
+//        startActivity(i);
+
+
+        try {
+            PackageManager pm = getPackageManager();
+            pm.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, "Whatsapp app not installed in your phone", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 }
